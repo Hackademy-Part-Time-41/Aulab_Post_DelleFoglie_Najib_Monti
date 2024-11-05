de81137f3ad9545215215a675ec191be4bc4cba7
Simone Delle Foglie <simone.asroma@outlook.it>
2024-11-05 19:28:14 +0100

    Scaffolding

--- aulab_post_DelleFoglie_Najib_Monti/routes/web.php
+++ aulab_post_DelleFoglie_Najib_Monti/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PubblicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get ('/', [PubblicController::class, 'homepage'])->name('homepage');
+
+
