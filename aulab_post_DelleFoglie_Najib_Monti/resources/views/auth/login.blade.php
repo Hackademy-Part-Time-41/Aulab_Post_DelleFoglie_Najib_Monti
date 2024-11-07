01bce19e91d0daa40c6129671c09fda9fa2b9080
Simone Delle Foglie <simone.asroma@outlook.it>
2024-11-07 17:35:55 +0100

    commit

--- aulab_post_DelleFoglie_Najib_Monti/resources/views/auth/login.blade.php
+++ aulab_post_DelleFoglie_Najib_Monti/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="container-fluid p-5 bg-secondary-subtle text-center">
         <div class="row justify-content-center">
-            <div class="cool-12">
+            <div class="col-12">
                 <h1 class="display-1">ACCEDI</h1>
             </div>
         </div>
