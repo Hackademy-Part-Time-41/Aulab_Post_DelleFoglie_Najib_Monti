0daa3a0756d391c8d4ac7c8b4b4b76b6033d5d37
Simone Delle Foglie <simone.asroma@outlook.it>
2024-11-13 15:59:25 +0100

    User Story 3 completata

--- aulab_post_DelleFoglie_Najib_Monti/app/Http/Controllers/ArticleController.php
+++ aulab_post_DelleFoglie_Najib_Monti/app/Http/Controllers/ArticleController.php
@@ -25,7 +25,7 @@ class ArticleController extends Controller implements HasMiddleware
      */
     public function index()
     {
-        $articles = Article::orderBy('created_at', 'desc')->get();
+        $articles = Article::where('is_accepted', true)->orderBy('created_at', 'desc')->get();
         return view('article.index', compact('articles'));
     }
 
@@ -65,13 +65,13 @@ class ArticleController extends Controller implements HasMiddleware
 
     public function byCategory(Category $category){
 
-        $articles = $category->articles()->orderBy('created_at', 'desc')->get();
+        $articles = $category->articles()->where('is_accepted', true)->orderBy('created_at', 'desc')->get();
         return view('article.byCategory', compact('articles', 'category'));
     }
 
     public function byUser(User $user){
 
-        $articles = $user->articles()->orderBy('created_at', 'desc')->get();
+        $articles = $user->articles()->where('is_accepted', true)->orderBy('created_at', 'desc')->get();
         return view('article.byUser', compact('articles', 'user'));
     }
 
--- aulab_post_DelleFoglie_Najib_Monti/app/Http/Controllers/RevisorController.php
+++ aulab_post_DelleFoglie_Najib_Monti/app/Http/Controllers/RevisorController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class RevisorController extends Controller
 {
-    //
+    public function dashboard(){
+        $unrevisionedArticles = Article::where('is_accepted', NULL)->get();
+        $acceptedArticles = Article::where('is_accepted', true)->get();
+        $rejectedArticles= Article::where('is_accepted', false)->get();
+        return view('revisor.dashboard', compact('unrevisionedArticles', 'acceptedArticles', 'rejectedArticles'));
+    }
+
+    public function acceptArticle(Article $article){
+        $article->is_accepted = true;
+        $article->save();
+        return redirect(route('revisor.dashboard'))->with('message', "Hai accettato l'articolo $article->title");
+    }
+
+    public function rejectArticle(Article $article){
+        $article->is_accepted = false;
+        $article->save();
+        return redirect(route('revisor.dashboard'))->with('message', "Hai rifiutato l'articolo $article->title");
+    }
+
+    public function undoArticle(Article $article){
+        $article->is_accepted = NULL;
+        $article->save();
+        return redirect(route('revisor.dashboard'))->with('message', "Hai rimandato in revisione l'articolo $article->title in revisione");
+    }
 }
