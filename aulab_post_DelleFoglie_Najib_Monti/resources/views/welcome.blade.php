c7690eb28a4d476b84d94cc9326ee71c152b3d9c
Mohamed Najib <cs1031sp@gmail.com>
2024-11-15 18:14:59 +0100

    pull

--- aulab_post_DelleFoglie_Najib_Monti/resources/views/welcome.blade.php
+++ aulab_post_DelleFoglie_Najib_Monti/resources/views/welcome.blade.php
@@ -16,52 +16,56 @@
             
             <div class="row wrap shadow align-items-center">
 
-              <div class="img-my col-12 col-md-11 col-lg-10 col-xl-7">
+              <div class="img-my col-12 col-md-11 col-lg-10 col-xl-6">
                 <img class="img-fluid" src="{{Storage::url('images/Designer_35.jpg')}}" alt="">
               </div>
 
-              <div class="col-12 col-md-11 col-lg-10 col-xl-5">
+              <div class="row justify-content-around col-12 col-md-9 col-xl-8 col-xxl-6">
 
-                <h2 class="text-center mb-3 fw-bold">Benvenuto nella nostra community</h2>
-                <p class="text-center ">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt numquam aliquid asperiores repellat distinctio possimus saepe nostrum quibusdam quis quod. Quisquam mollitia tempora nostrum qui culpa recusandae, nisi assumenda cupiditate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni corrupti harum necessitatibus laborum possimus repudiandae veniam, ut natus perferendis fugit dicta delectus architecto, distinctio eos odit dolorem, tempora impedit hic.
-                </p>
+                <div class="col-12 col-xl-4 mt-3">
+                  <h2 class="text-center mb-3 fw-bold">Benvenuto nella nostra community</h2>
+                  <p class="text-center ">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt numquam aliquid asperiores repellat distinctio possimus saepe nostrum quibusdam quis quod. Quisquam mollitia tempora nostrum qui culpa recusandae, nisi assumenda cupiditate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni corrupti harum necessitatibus laborum possimus repudiandae veniam, ut natus perferendis fugit dicta delectus architecto, distinctio eos odit dolorem, tempora impedit hic.
+                  </p>
+                </div>
 
-                <div class="row justify-content-center">
-                    @foreach ($articles as $article)
-                      <div class="shadow postIt  col-10 col-md-3 col-lg-2">
-                        <div class=""  style="min-width: 8rem">
-                          {{-- <img src="{{Storage::url($article->image)}}" class="card-img-top mt-2" alt="articolo:{{$article->title}}"> --}}
-                          <img src="https://picsum.photos/400/400" class="card-img-top mt-2" alt="articolo:{{$article->title}}">
-                          
-                          <div class="">
-      
-                            <h4 class="text-center postIt-text">{{$article->title}}</h4>
-                            <p class="text-center postIt-text">{{$article->subtitle}}</p>
-                            <p class="text-center">
-                              <a href="{{ route('article.byCategory', $article->category)}}" class="postIt-text">{{$article->category->name}}</a>
-                            </p>
-      
-                          </div>
-                          <div class="text-center">
-                                  <a href="{{ route('article.show', $article)}}" class="postIt-text my-btn">Leggi</a>
-                          </div>
+                <div class="col-12 col-xl-6 mt-3">
+                  <h3 class="text-center fw-bold">Crea anche te il tuo Post-it</h3>
+                  <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum at libero tempora vel unde dolor similique itaque magnam aut dicta saepe error, eveniet soluta molestias, delectus, perferendis aliquid. Incidunt, ipsam.</p>
+                  <div class="row justify-content-center">
+                    <div class="col-12">
+                      <a href="{{route('article.create')}}" class="btn my-btn d-block">Crea Post-it</a>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12 row justify-content-around">
+                  @foreach ($articles as $article)
+                    <div class="shadow postIt  col-10 col-md-3 col-lg-2">
+                      <div class=""  style="min-width: 8rem">
+                        {{-- <img src="{{Storage::url($article->image)}}" class="card-img-top mt-2" alt="articolo:{{$article->title}}"> --}}
+                        <img src="https://picsum.photos/400/400" class="card-img-top mt-2" alt="articolo:{{$article->title}}">
+                        
+                        <div class="">
+    
+                          <h4 class="text-center postIt-text">{{$article->title}}</h4>
+                          <p class="text-center postIt-text">{{$article->subtitle}}</p>
+                          <p class="text-center">
+                            <a href="{{ route('article.byCategory', $article->category)}}" class="postIt-text">{{$article->category->name}}</a>
+                          </p>
+    
+                        </div>
+                        <div class="text-center">
+                                <a href="{{ route('article.show', $article)}}" class="postIt-text my-btn">Leggi</a>
                         </div>
                       </div>
-                    @endforeach
-                </div>
+                    </div>
+                  @endforeach
+              </div>
 
               </div>
 
-              <div class="col-xl-4">
-              <h3 class="text-center fw-bold">Crea anche te il tuo Post-it</h3>
-              <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum at libero tempora vel unde dolor similique itaque magnam aut dicta saepe error, eveniet soluta molestias, delectus, perferendis aliquid. Incidunt, ipsam.</p>
-              <div class="row justify-content-center">
-                <div class="col-12">
-                  <a href="{{route('article.create')}}" class="btn my-btn d-block">Crea Post-it</a>
-                </div>
-              </div>
-            </div>
+              
           
             </div>
 
