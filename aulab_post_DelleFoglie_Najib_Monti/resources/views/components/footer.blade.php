d9dd7c4e545fa8f5e02f10476e24f9213c226aaa
Simone Delle Foglie <simone.asroma@outlook.it>
2024-11-25 11:42:25 +0100

    pull

--- aulab_post_DelleFoglie_Najib_Monti/resources/views/components/footer.blade.php
+++ aulab_post_DelleFoglie_Najib_Monti/resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
-<xlayout>
+<div class="container-fluid">
     <footer class="corallo-chiaro text-black mt-3">
-        <div class="container-fluid">
+        <div class="">
             <div class="row justify-content-between align-items-center">
                 <!-- Social Media Links (a sinistra) -->
                 <div class="col-4 text-left">
@@ -10,8 +10,12 @@
                     <a href="https://www.instagram.com/il_p.ost_it/" target="_blank" rel="noopener noreferrer" class="mx-2">
                         <img src="{{ Storage::url('images/logo-instagram.png') }}" alt="Instagram" class="social-icon">
                     </a>
+                    <div class="col-6 row">
+                    <h5>Sede Legale</h5>
+                    <p>29121 Piacenza, Italia</p>
+                    </div>
                 </div>
-    
+                
                 <!-- Copyright (al centro) -->
                 <div class="col-4 text-center">
                     <p>&copy; 2024 Il Post-it. Tutti i diritti riservati.</p>
@@ -19,10 +23,7 @@
     
                 <!-- Sede Legale e Contatti (a destra) -->
                 <div class= "row col-4">
-                    <div class="col-6">
-                    <h5>Sede Legale</h5>
-                    <p>29121 Piacenza, Italia</p>
-                    </div>
+                    
                     <div class="col-6">
                     <h5>Contatti</h5>
                     <p>Email: <a href="mailto:info@tuosito.com" class="text-dark">info@tuosito.com</a></p>
@@ -32,7 +33,7 @@
             </div>
         </div>
     </footer>
-</xlayout>
+</div>
 
 
 
